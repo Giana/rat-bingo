8d6a5b564648ce1e6a7485b51f5f6a844db52cec
gnguarascio <42662834+gnguarascio@users.noreply.github.com>
2019-12-28 00:24:17 -0500

    Tile constructor + getters/setters

--- src/Tile.java
+++ src/Tile.java
@@ -1,3 +1,32 @@
 public class Tile
 {
+    private String letter;
+    private int number;
+    private boolean called;
+    private boolean selected;
+
+    // Constructor
+    // Creates a new bingo tile
+    public Tile(String letter, int number)
+    {
+        this.letter = letter;
+        this.number = number;
+    }
+
+    // Various getters & setters
+    public String getLetter() { return letter; }
+
+    public void setLetter(String letter) { this.letter = letter; }
+
+    public int getNumber() { return number; }
+
+    public void setNumber(int number) { this.number = number; }
+
+    public boolean getCalled() { return called; }
+
+    public void setCalled(boolean called) { this.called = called; }
+
+    public boolean getSelected() { return selected; }
+
+    public void setSelected(boolean selected) { this.selected = selected; }
 }
