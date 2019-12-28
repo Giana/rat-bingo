bf7cc4fdc480e5a46b5d2bc41e488e990d4521b8
gnguarascio <42662834+gnguarascio@users.noreply.github.com>
2019-12-28 15:41:02 -0500

    Minor adjustments to tile

--- src/Board.java
+++ src/Board.java
@@ -1,3 +1,12 @@
+import java.util.ArrayList;
+
 public class Board
 {
+    private int size = 5;
+    private ArrayList<Integer> numbers = new ArrayList<>();   // Holds numbers on board
+
+    // Default constructor
+    public Board() { }
+
+
 }
--- src/Tile.java
+++ src/Tile.java
@@ -1,23 +1,19 @@
 public class Tile
 {
-    private String letter;
     private int number;
     private boolean called;
     private boolean selected;
 
-    // Constructor
-    // Creates a new bingo tile
-    public Tile(String letter, int number)
+    // Default constructor
+    public Tile() { }
+
+    // Parameterized constructor
+    public Tile(int number)
     {
-        this.letter = letter;
         this.number = number;
     }
 
     // Various getters & setters
-    public String getLetter() { return letter; }
-
-    public void setLetter(String letter) { this.letter = letter; }
-
     public int getNumber() { return number; }
 
     public void setNumber(int number) { this.number = number; }
