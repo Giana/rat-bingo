608f360040a911318db46bc5bdd88e3438dda6dd
gnguarascio <42662834+gnguarascio@users.noreply.github.com>
2019-12-28 21:30:35 -0500

    Finished all game win checks in Game (untested)

--- src/Game.java
+++ src/Game.java
@@ -31,15 +31,16 @@ public class Game
     // Checks for a horizontal win
     public String checkHorizontal(Board board)
     {
+        int size = board.getSize();
         int rowCount = 0;
         String state = "N/A";
 
-        while(rowCount < 5)
+        while(rowCount < size)
         {
-            for(int i = 0; i < 5; i++)
+            for(int i = 0; i < size; i++)
             {
-                if(board.getMap()[rowCount][i].getCalled() == true &&
-                        board.getMap()[rowCount][i].getSelected() == true)
+                if(board.getMap()[rowCount][i].getCalled() &&
+                        board.getMap()[rowCount][i].getSelected())
                 {
                     state = "horizonal";
                 }
@@ -59,15 +60,16 @@ public class Game
     // Checks for a verticle win
     public String checkVerticle(Board board)
     {
+        int size = board.getSize();
         int columnCount = 0;
         String state = "N/A";
 
-        while(columnCount < 5)
+        while(columnCount < size)
         {
-            for(int i = 0; i < 5; i++)
+            for(int i = 0; i < size; i++)
             {
-                if(board.getMap()[i][columnCount].getCalled() == true &&
-                        board.getMap()[i][columnCount].getSelected() == true)
+                if(board.getMap()[i][columnCount].getCalled() &&
+                        board.getMap()[i][columnCount].getSelected())
                 {
                     state = "verticle";
                 }
@@ -79,6 +81,256 @@ public class Game
             }
 
             columnCount++;
+        }
+
+        return state;
+    }
+
+    // Check for a diagonal win
+    public String checkDiagonal(Board board)
+    {
+        int size = board.getSize();
+        int j = 4;
+        String state = "N/A";
+        String rightState = "N/A";
+        String leftState = "N/A";
+
+        // Check from top left
+        for(int i = 0; i < size; i++)
+        {
+            if(board.getMap()[i][i].getCalled() &&
+                    board.getMap()[i][i].getSelected())
+            {
+                leftState = "diagonal";
+            }
+            else
+            {
+                leftState = "N/A";
+                break;
+            }
+        }
+
+        // Check from top right
+        for(int i = 0; i < size; i++)
+        {
+            if(board.getMap()[i][j].getCalled() &&
+                    board.getMap()[i][j].getSelected())
+            {
+                rightState = "diagonal";
+                j--;
+            }
+            else
+            {
+                rightState = "N/A";
+                break;
+            }
+        }
+
+        if(leftState == "diagonal" || rightState == "diagonal")
+        {
+            state = "diagonal";
+        }
+
+        return state;
+    }
+
+    // Check for an X win
+    public String checkX(Board board)
+    {
+        int size = board.getSize();
+        int j = 4;
+        String state = "N/A";
+        String rightState = "N/A";
+        String leftState = "N/A";
+
+        // Check from top left
+        for(int i = 0; i < size; i++)
+        {
+            if(board.getMap()[i][i].getCalled() &&
+                    board.getMap()[i][i].getSelected())
+            {
+                leftState = "diagonal";
+            }
+            else
+            {
+                leftState = "N/A";
+                break;
+            }
+        }
+
+        // Check from top right
+        for(int i = 0; i < size; i++)
+        {
+            if(board.getMap()[i][j].getCalled() &&
+                    board.getMap()[i][j].getSelected())
+            {
+                rightState = "diagonal";
+                j--;
+            }
+            else
+            {
+                rightState = "N/A";
+                break;
+            }
+        }
+
+        // Check for X conditions
+        if(leftState == "diagonal" && rightState == "diagonal")
+        {
+            state = "x";
+        }
+
+        return state;
+    }
+
+    // Check for a four corners win
+    public String checkCorners(Board board)
+    {
+        int lowerBound = 0;
+        int upperBound = board.getSize();
+        String state = "N/A";
+
+        if(board.getMap()[lowerBound][lowerBound].getCalled() &&
+                board.getMap()[lowerBound][lowerBound].getSelected() &&
+                board.getMap()[lowerBound][upperBound].getCalled() &&
+                board.getMap()[lowerBound][upperBound].getSelected() &&
+                board.getMap()[upperBound][lowerBound].getCalled() &&
+                board.getMap()[upperBound][lowerBound].getSelected() &&
+                board.getMap()[upperBound][upperBound].getCalled() &&
+                board.getMap()[upperBound][upperBound].getSelected())
+        {
+            state = "corners";
+        }
+
+        return state;
+    }
+
+    // Check for a T win
+    public String checkT(Board board)
+    {
+        int size = board.getSize();
+        int row = 0;
+        int column = (int) Math.floor(size / 2);
+        String state = "N/A";
+        String horizontalState = "N/A";
+        String verticleState = "N/A";
+
+        // Check horizontal
+        for(int i = 0; i < size; i++)
+        {
+            if(board.getMap()[row][i].getCalled() &&
+                    board.getMap()[row][i].getSelected())
+            {
+                horizontalState = "horizonal";
+            }
+            else
+            {
+                horizontalState = "N/A";
+                break;
+            }
+        }
+        // Check verticle
+        for(int i = 0; i < size; i++)
+        {
+            if(board.getMap()[i][column].getCalled() &&
+                    board.getMap()[i][column].getSelected())
+            {
+                verticleState = "verticle";
+            }
+            else
+            {
+                verticleState = "N/A";
+                break;
+            }
+        }
+
+        // Check for T conditions
+        if(horizontalState == "horizontal" && verticleState == "verticle")
+        {
+            state = "t";
+        }
+
+        return state;
+    }
+
+    // Check for L win
+    public String checkL(Board board)
+    {
+        int size = board.getSize();
+        int row = (size - 1);
+        int column = 0;
+        String state = "N/A";
+        String horizontalState = "N/A";
+        String verticleState = "N/A";
+
+        // Check horizontal
+        for(int i = 0; i < size; i++)
+        {
+            if(board.getMap()[row][i].getCalled() &&
+                    board.getMap()[row][i].getSelected())
+            {
+                horizontalState = "horizonal";
+            }
+            else
+            {
+                horizontalState = "N/A";
+                break;
+            }
+        }
+        // Check verticle
+        for(int i = 0; i < size; i++)
+        {
+            if(board.getMap()[i][column].getCalled() &&
+                    board.getMap()[i][column].getSelected())
+            {
+                verticleState = "verticle";
+            }
+            else
+            {
+                verticleState = "N/A";
+                break;
+            }
+        }
+
+        // Check for T conditions
+        if(horizontalState == "horizontal" && verticleState == "verticle")
+        {
+            state = "t";
+        }
+
+        return state;
+    }
+
+    // Check for all win
+    public String checkAll(Board board)
+    {
+        int size = board.getSize();
+        int rowCount = 0;
+        String horizontalState = "N/A";
+        String state = "N/A";
+
+        while(rowCount < size)
+        {
+            for(int i = 0; i < size; i++)
+            {
+                if(board.getMap()[rowCount][i].getCalled() &&
+                        board.getMap()[rowCount][i].getSelected())
+                {
+                    horizontalState = "horizontal";
+                }
+                else
+                {
+                    state = "N/A";
+                    break;
+                }
+            }
+
+            rowCount++;
+        }
+
+        if(horizontalState == "horizontal")
+        {
+            state = "all";
         }
 
         return state;
