93e51564ea7cb42c226c67265ee94e530397a84f
gnguarascio <42662834+gnguarascio@users.noreply.github.com>
2019-12-30 13:44:46 -0500

    Adjusted incremental variables in numbersGenerator()... for fun, mostly

--- src/Board.java
+++ src/Board.java
@@ -24,7 +24,7 @@ public class Board
     // Randomly selects numbers for the board
     public void numbersGenerator()
     {
-        int i = 0, j = 0, k = 0, m = 0, n = 0;
+        int b = 0, i = 0, n = 0, g = 0, o = 0;
 
         // Ranges for letters
         int bMin = 1, bMax = 15;
@@ -34,67 +34,67 @@ public class Board
         int oMin = 61, oMax = 75;
 
         // Generate Bs
-        while(i < 5)
+        while(b < 5)
         {
             int rand = (int) (Math.random() * ((bMax - bMin) + 1)) + bMin;
 
             if(!numbers.contains(rand))
             {
                 numbers.add(rand);
-                map[i][0] = new Tile(rand);
-                i++;
+                map[b][0] = new Tile(rand);
+                b++;
             }
         }
 
         // Generate Is
-        while(j < 5)
+        while(i < 5)
         {
             int rand = (int) (Math.random() * ((iMax - iMin) + 1)) + iMin;
 
             if(!numbers.contains(rand))
             {
                 numbers.add(rand);
-                map[j][1] = new Tile(rand);
-                j++;
+                map[i][1] = new Tile(rand);
+                i++;
             }
         }
 
         // Generate Ns
-        while(k < 5)
+        while(n < 5)
         {
             int rand = (int) (Math.random() * ((nMax - nMin) + 1)) + nMin;
 
             if(!numbers.contains(rand))
             {
                 numbers.add(rand);
-                map[k][2] = new Tile(rand);
-                k++;
+                map[n][2] = new Tile(rand);
+                n++;
             }
         }
 
         // Generate Gs
-        while(m < 5)
+        while(g < 5)
         {
             int rand = (int) (Math.random() * ((gMax - gMin) + 1)) + gMin;
 
             if(!numbers.contains(rand))
             {
                 numbers.add(rand);
-                map[m][3] = new Tile(rand);
-                m++;
+                map[g][3] = new Tile(rand);
+                g++;
             }
         }
 
         // Generate Os
-        while(n < 5)
+        while(o < 5)
         {
             int rand = (int) (Math.random() * ((oMax - oMin) + 1)) + oMin;
 
             if(!numbers.contains(rand))
             {
                 numbers.add(rand);
-                map[n][4] = new Tile(rand);
-                n++;
+                map[o][4] = new Tile(rand);
+                o++;
             }
         }
     }
