bd719bb745c00841ef2aca36ee4da87e7b1daf52
gnguarascio <42662834+gnguarascio@users.noreply.github.com>
2020-01-01 22:22:55 -0500

    Some GUI progress

---
+++ src/GUI/GUI.java
@@ -0,0 +1,10 @@
+package GUI;
+
+import javax.swing.*;
+
+public class GUI {
+    private JPanel parentPanel;
+    private JPanel leftMenuPanel;
+    private JPanel rightMenuPanel;
+    private JPanel switchPanel;
+}
