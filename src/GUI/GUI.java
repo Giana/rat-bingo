15b03c2cf594ca7fc62c456c3dc256a60365a776
gnguarascio <42662834+gnguarascio@users.noreply.github.com>
2020-01-01 22:58:27 -0500

    GUI progress

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -2,9 +2,15 @@ package GUI;
 
 import javax.swing.*;
 
-public class GUI {
+public class GUI
+{
     private JPanel parentPanel;
     private JPanel leftMenuPanel;
     private JPanel rightMenuPanel;
     private JPanel switchPanel;
+    private JPanel mainMenuPanel;
+    private JPanel gamePanel;
+    private JPanel winPanel;
+    private JPanel lossPanel;
+    private JLabel ratBingoLogoImage;
 }
