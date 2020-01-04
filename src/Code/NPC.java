cb84ba7f2079b059d9ae0a0c606c09c1ffbf2600
gnguarascio <42662834+gnguarascio@users.noreply.github.com>
2020-01-04 16:24:33 -0500

    Working on NPC class

--- src/Code/NPC.java
+++ src/Code/NPC.java
@@ -2,4 +2,46 @@ package Code;
 
 public class NPC
 {
+    // Default constructor
+    public NPC() { }
+
+    public boolean checkForTileValue(int tileValue, Game currentGame)
+    {
+        int size = currentGame.getNpcBoard().getSize();
+
+        // Check for a B
+        if(tileValue >= 1 && tileValue <= 15)
+        {
+
+        }
+        // Check for an I
+        else if(tileValue >= 16 && tileValue <= 30)
+        {
+
+        }
+        // Check for N
+        else if(tileValue >= 31 && tileValue <= 45)
+        {
+
+        }
+        // Check for G
+        else if(tileValue >= 46 && tileValue <= 60)
+        {
+
+        }
+        // Check for O
+        else if(tileValue >= 61 && tileValue <= 75)
+        {
+
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void markTile()
+    {
+
+    }
 }
